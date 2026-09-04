57c513acedb2008bbf3f8de45688ffe84d196b89
Aluno <aluno@academico.camaqua>
2026-09-03 21:57:03 -0300

    Continuação da atividade 17

--- Atividades/17. Funcoes e Eventos JS/script.js
+++ Atividades/17. Funcoes e Eventos JS/script.js
@@ -1,9 +1,14 @@
+/**
+ * Muda cor do background
+ * @param nomeCor 
+ */
+
 function alterarPageColor(nomeCor){
     document.body.style.backgroundColor = `${nomeCor}`;
 }
 
 function selecionarBotao(){
-    let btns = document.querySelectorAll('.btn');
+    let btns = document.querySelectorAll("#altera-bg .btn");
     let nomeCor;
     btns.forEach(function(btn){
         btn.addEventListener("click", function(event){
@@ -15,3 +20,100 @@ function selecionarBotao(){
 }
 
 selecionarBotao();
+
+/**
+ * Contador
+ * 
+ */
+function contador(){
+    let btnMenos = document.getElementById("menos");
+    let inputMostraNumero = document.getElementById("mostra-numero");
+    let btnMais = document.getElementById("mais");
+
+    inputMostraNumero.value = 0;
+    let valorCampo = 0;
+
+    [btnMenos, btnMais].forEach(function(btn){
+        btn.addEventListener("click", function(event){
+            
+            if (event.target.id === "mais") {
+                inputMostraNumero.value = ++valorCampo;
+            } else {
+                inputMostraNumero.value = --valorCampo;
+            }
+
+            if (valorCampo > 5) {                
+                inputMostraNumero.style.color = "red";
+                inputMostraNumero.style.fontWeight = 600;
+            } else {
+                inputMostraNumero.style.color = "black";
+                inputMostraNumero.style.fontWeight = 400;
+            }
+        })        
+    });
+}
+
+contador();
+
+/**
+ * Visibilidade do texto
+ */
+
+function visibilidadeTexto(){
+    let texto = document.getElementById("texto");
+    let btnEsconder = document.getElementById("esconder");
+    let btnMostrar = document.getElementById("mostrar");
+
+    let textoParagrafo = texto.innerHTML;
+    
+    [btnEsconder, btnMostrar].forEach(function(btns) {
+        btns.addEventListener("click", function(event){
+            event.target.id === "esconder" ?
+            texto.innerHTML = "..." :
+            texto.innerHTML = textoParagrafo;
+        })
+    })
+
+
+}
+
+visibilidadeTexto();
+
+/**
+ * Tecla pressionada no campo
+ */
+
+function teclaPressionada(e){
+    console.log(e.key)
+    alert(`Você pressionou uma tecla ${e.key}`);
+}
+
+/**
+ * Escrever input na tela
+ */
+
+function escreveTextoDigitado() {
+    let inputTexto = document.getElementById('caixa-texto');
+    let paragrafo = document.getElementById('paragrafo');
+    let textoDigitado = "";
+    let posicaoCursor = 0;
+
+    inputTexto.addEventListener("keydown", function(e){
+        posicaoCursor = inputTexto.selectionEnd;
+        if (e.key.length > 1) {
+            if (e.key === "Backspace") {
+                textoDigitado = textoDigitado.replace(
+                    (textoDigitado.charAt(posicaoCursor - 1))
+                    ,"");
+                //"string".replace("string".charAt("string".length - 1),"")
+            }
+        } else {
+            textoDigitado += e.key;
+        }
+
+        paragrafo.innerHTML = textoDigitado;
+
+    })
+}
+
+escreveTextoDigitado()
